4dfd7ff05d3cac2d53c0f36b82d47921b6c46643
Firestorm01X2 <firestorm01x2@gmail.com>
2023-09-23 11:55:28 +0300

    Добавил файлы в проект

---
+++ WinFormsTest/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btTest = new Button();
+            SuspendLayout();
+            // 
+            // btTest
+            // 
+            btTest.Location = new Point(401, 162);
+            btTest.Name = "btTest";
+            btTest.Size = new Size(75, 23);
+            btTest.TabIndex = 0;
+            btTest.Text = "button1";
+            btTest.UseVisualStyleBackColor = true;
+            btTest.Click += btTest_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btTest);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btTest;
+    }
+}
